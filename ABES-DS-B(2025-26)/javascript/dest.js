d584eb27eae0f17554e90e9c3090bc6c28206491
Saqib-0007 <saqibahmedkhan1133@gmail.com>
2025-10-14 11:19:53 +0530

    new files

---
+++ ABES-DS-B(2025-26)/javascript/dest.js
@@ -0,0 +1,23 @@
+const library = [
+            {
+                id : 1, title : 'the Hobbit', author : 'J.R.R. Tolkien'
+            },
+            {
+                id : 2, title : '1984', author : 'George Orwell'
+            },
+            {
+                id : 3, title : 'To Kill a Mockingbird', author : 'Harper Lee'
+            },
+            {
+                id : 4, title : 'Pride and Prejudice', author : 'Jane Austen'
+            },
+            {
+                id : 5, title : 'The Great Gatsby', author : 'F. Scott Fitzgerald'
+            }
+        ]
+
+        // all books
+        console.log(library);
+
+        // first book
+        console.log(library[0]);
